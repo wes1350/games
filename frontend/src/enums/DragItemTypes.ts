1f4d92071e0b2121b47e54be903c25986b548950
Wesley Runnels <wrunnels@mit.edu>
2021-10-09 16:14:17 -0400

    WIP revert monorepo

--- frontend/src/enums/DragItemTypes.ts
+++
@@ -1,3 +0,0 @@
-export const DragItemTypes = {
-    DOMINO: "DOMINO"
-};
--- frontend/src/view/AboutPage.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-export const AboutPage = () => {
-    return (
-        <div className="About">
-            <span>You can play Dominoes on this page.</span>
-        </div>
-    );
-};
